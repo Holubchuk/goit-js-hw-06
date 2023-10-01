297e5115cbf4057edb8e3cbbbad04f3d32567373
Holubchuk <sashagolubchuk3@gmail.com>
2023-10-01 16:28:02 -0400

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+
+ingredients.forEach(item => {
+  const tag = document.createElement('li');
+    tag.classList.add ('item');
+    tag.textContent = item;
+    list.append(tag);
+})
