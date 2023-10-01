e51029aee4b0960bd38230618d605fd1013a1e22
Holubchuk <sashagolubchuk3@gmail.com>
2023-10-01 19:56:22 -0400

    task-4

---
+++ js/task-04.js
@@ -0,0 +1,20 @@
+let counterValue = 0;
+
+const elementValue = document.querySelector('#value');
+const decrementBtn = document.querySelector('#counter button[data-action="decrement"]');
+const incrementBtn = document.querySelector('#counter button[data-action="increment"]');
+
+
+decrementBtn.addEventListener("click", () => {
+    counterValue -= 1;
+    elementValue.textContent = counterValue;
+});
+
+incrementBtn.addEventListener("click", () => {
+    counterValue += 1;
+    elementValue.textContent = counterValue;
+});
+
+
+
+
